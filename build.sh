a555622685286f484f5e17f4e3ea688bcfcbb914
Renat <70426913+renat2006@users.noreply.github.com>
2024-03-12 11:12:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements/prod.txt
+pip install -r requirements/dev.txt
+pip install -r requirements/test.txt
 cd skyparcel
 # Convert static asset files
 python manage.py collectstatic --no-input
